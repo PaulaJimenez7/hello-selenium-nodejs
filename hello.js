95e6f3aecad99a715bfe2d40ece6d532ba962a65
Paula Jimenez <pjimenezm@viewnext.com>
2021-02-02 12:04:04 +0100

    Elimino pop up y realizo una busqueda en google

--- hello.js
+++ hello.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
-const webdriver = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function hello(){
-	let driver = new webdriver.Builder()
+	let driver = await new Builder()
 		.forBrowser('firefox').build();
 	await driver.get('https://www.google.com/');
+
+	await driver.switchTo().frame(0);
+	await driver.findElement(By.id('introAgreeButton')).click();
+
+	await driver.switchTo().defaultContent();
+	await driver.findElement(By.name('q')).sendKeys('devops',Key.ENTER);
+
 })();
